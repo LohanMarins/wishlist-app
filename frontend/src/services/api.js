a8c99c0ef94310894681556e05c5a5696c787520
LohanMarins <86624954+LohanMarins@users.noreply.github.com>
2026-01-05 20:22:14 +0100

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000";
+const API_URL = "https://wishlist-backend-1x5u.onrender.com";
 
 export async function getItems(owner) {
     const res = await fetch(`${API_URL}/items?owner=${owner}`);
