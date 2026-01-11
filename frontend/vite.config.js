2dac6bc9fe55d8ab4e02fefd947b5fc9b0e94cc6
LohanMarins <lohan.lam91@gmail.com>
2026-01-11 18:44:09 +0100

    "agora vai"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    dedupe: ["react", "react-dom"]
+  },
+  optimizeDeps: {
+    include: ["react/jsx-runtime"]
+  }
 });
