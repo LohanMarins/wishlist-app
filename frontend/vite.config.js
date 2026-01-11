5c445bcf02363e024cd3cc736705161b8d175fb7
LohanMarins <lohan.lam91@gmail.com>
2026-01-11 19:07:28 +0100

    "rf"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    dedupe: ["react", "react-dom"]
-  },
-  optimizeDeps: {
-    include: ["react", "react-dom", "react/jsx-runtime"]
-  },
-  build: {
-    rollupOptions: {
-      external: []
-    },
-    commonjsOptions: {
-      include: [/node_modules/]
-    }
-  }
+  plugins: [react()]
 });
